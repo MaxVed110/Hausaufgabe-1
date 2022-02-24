2731b4ec3b80899ee15d45885f2bbff72461e44e
Maxim <vedernikovmaxim@rambler.ru>
2022-02-24 23:09:24 +0300

    создана программа сравнения 3-х файлов

---
+++ Сравнение_3х_чисел/Program.cs
@@ -0,0 +1,30 @@
+﻿int numberA = new int();
+int numberB = new int();
+int numberC = new int();
+
+Console.Write("Введите первое число: ");
+numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if(numberB > max)
+{
+    max = numberB;
+    if(numberC > max) max = numberC;
+    else max = numberB;
+}
+else
+{
+    if(numberC > max) max = numberC;
+    else max = numberA;
+}
+
+if (numberA == numberB && numberB == numberC) Console.WriteLine("Числа равны");
+else Console.WriteLine("Наибольшее число: " + max.ToString());
+  
