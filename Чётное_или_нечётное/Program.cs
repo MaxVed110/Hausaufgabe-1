d72fb698ef88283cc9706b44869d98e254cdd203
Maxim <vedernikovmaxim@rambler.ru>
2022-02-24 23:20:05 +0300

    создана программа определения чётности числа

---
+++ Чётное_или_нечётное/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberA = new int();
+
+Console.Write("Введите число: ");
+
+numberA = Convert.ToInt32(Console.ReadLine());
+
+if((numberA % 2) == 0) Console.WriteLine("Число четное ");
+else Console.WriteLine("Число нечетное ");
+
+
