0d290e8c2b3394ece991824d2f0b824af56c482d
Maxim <vedernikovmaxim@rambler.ru>
2022-02-24 22:43:34 +0300

    создана программа сравнения 2-х файлов

---
+++ Сравнение_2х_чисел/Program.cs
@@ -0,0 +1,12 @@
+﻿int numberA = new int();
+int numberB = new int();
+
+Console.Write("Введите число №1: ");
+numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число №2: ");
+numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB) Console.WriteLine("Большее число: " + numberA.ToString());
+if (numberB > numberA) Console.WriteLine("Большее число: " + numberB.ToString());
+if (numberA == numberB) Console.WriteLine("Числа равны");
