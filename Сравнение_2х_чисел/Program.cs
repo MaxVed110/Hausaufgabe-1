25f4c1d867349d9f661e60afc1223de873c238e8
Maxim <vedernikovmaxim@rambler.ru>
2022-02-24 23:13:01 +0300

    изменена программа сравнения 2-х файлов

--- Сравнение_2х_чисел/Program.cs
+++ Сравнение_2х_чисел/Program.cs
@@ -7,6 +7,15 @@ numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число №2: ");
 numberB = Convert.ToInt32(Console.ReadLine());
 
-if (numberA > numberB) Console.WriteLine("Большее число: " + numberA.ToString());
-if (numberB > numberA) Console.WriteLine("Большее число: " + numberB.ToString());
+if (numberA > numberB) 
+{
+    Console.WriteLine("Большее число: " + numberA.ToString());
+    Console.WriteLine("Меньшее число: " + numberB.ToString());
+}
+if (numberB > numberA) 
+{
+    Console.WriteLine("Большее число: " + numberB.ToString());
+    Console.WriteLine("Меньшее число: " + numberA.ToString());
+}
+
 if (numberA == numberB) Console.WriteLine("Числа равны");
