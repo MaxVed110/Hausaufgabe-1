e2b826d06fab8e93f4b07b234e041d77818929e5
Maxim <vedernikovmaxim@rambler.ru>
2022-02-25 21:22:10 +0300

    Создана программа вывода чётных чисел

---
+++ Чётные_числа_от_1_до_N/Program.cs
@@ -0,0 +1,21 @@
+﻿int number = new int();
+int count = new int();
+
+count = 2;
+
+Console.Write("Введите целое положительное число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 2)
+{
+    Console.Write("Чётные числа: ");
+    while (count <= number)
+    {
+        Console.Write(count.ToString() + ", ");
+        count = count + 2;
+    }
+}
+else 
+{
+    Console.WriteLine("Чётных чисел нет");
+}
